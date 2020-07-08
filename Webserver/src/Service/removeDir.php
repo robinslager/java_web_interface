8457bca9cd3c285af7d8785b9dab06e5f87c6e66
Robin Slager <robin.slager@northchain.tech>
2020-07-09 00:22:21 +0200

    fixed docker and autoload with composer.json

--- Webserver/src/Service/removeDir.php
+++ Webserver/src/Service/removeDir.php
@@ -1,10 +1,5 @@
 <?php
 
-
-namespace App\Service;
-
-use Symfony\Component\Finder\Finder;
-
 class removeDir
 {
 
@@ -17,29 +12,6 @@ class removeDir
     }
 
 
-    function rrmdir($dir)
-    {
-        if (is_dir($dir))
-        {
-            $objects = scandir($dir);
-            foreach ($objects as $object)
-            {
-
-                if ($object != '.' && $object != '..')
-                {
-                    dump($object);
-                    dump(is_dir($object));
-                    if(!is_dir($object)){
-                        dump(unlink($dir . "/" . $object));
-                    } else {
-                        
-                        $this->rrmdir($dir . "/" . $object);
-                    }
-                }
-            }
-            rmdir($dir);
-        }
-    }
 
 
 }
